927f4463f7084ce6b6704ee98a009fb7383981ed
SAFVAN-C-M-C <139871174+SAFVAN-C-M-C@users.noreply.github.com>
2023-07-26 23:09:26 +0530

    First commit

---
+++ Script.js
@@ -0,0 +1,63 @@
+var search = document.getElementById("searchBar");
+var slash = document.getElementById("slash");
+var menu =document.getElementById("burger");
+var drawer=document.getElementById("drawer");
+var menuClick = 0;
+var moonClick =0;
+console.log(menu)
+console.log(slash);
+console.log(drawer);
+search.onclick=function clicked(){
+    search.setAttribute('id','clicked');
+    slash.style.visibility = "hidden";
+}
+
+search.onblur = function() {
+    search.setAttribute('id', 'searchBar');
+    slash.style.visibility = "visible";
+}
+
+
+menu.onclick=function menuclick(){
+    menuClick++;
+    if(menuClick%2==1){
+        drawer.style.display = "block";
+    }
+    else{
+        drawer.style.display = "none";
+    }
+}
+drawer.onblur = function(){
+    drawer.style.display = "none";
+}
+var moonSun = document.getElementById("moon");
+var tryAng = document.getElementById('triangle');
+var bG = document.getElementsByTagName("body");
+var cardText1 = document.getElementById("card-text1");
+var cardText2 = document.getElementById("card-text2");
+var cardText3 = document.getElementById("card-text3");
+var webDev = document.getElementById("webDev");
+var learnMore = document.getElementById("learn");
+moonSun.onclick = function(){
+    moonClick++;
+    if(moonClick%2==0){
+        moonSun.innerHTML = "dark_mode"
+        bG[0].style.backgroundColor="#fff";
+        cardText1.style.color = "rgb(68, 68, 68)";
+        cardText2.style.color = "rgb(68, 68, 68)";
+        cardText3.style.color = "rgb(68, 68, 68)";
+        webDev.style.color = "#000";
+        tryAng.style.backgroundColor = "rgb(255, 255, 255)";
+        learnMore.style.backgroundColor = '#fff';
+    }
+    else{
+        moonSun.innerHTML = "light_mode"
+        bG[0].style.backgroundColor="#303030";
+        cardText1.style.color = "#fff";
+        cardText2.style.color = "#fff";
+        cardText3.style.color = "#fff";
+        webDev.style.color = "#fff";
+        tryAng.style.backgroundColor = '#3b3b3b';
+        learnMore.style.backgroundColor = '#3b3b3b';
+    }
+}
